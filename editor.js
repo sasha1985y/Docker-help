f3c62478e484c811d1befbe5ec9c7f00826e970d
sasha1985y <kuyancev1985@mail.ru>
2025-07-15 18:38:07 +0300

    -nth commit

---
+++ editor.js
@@ -0,0 +1,14 @@
+const ws = new WebSocket('ws://localhost:3000');
+
+const editablePre = document.getElementById('editablePre');
+
+// Загрузка содержимого index.html при загрузке страницы
+fetch('swarm_docker_secret.html')
+    .then(response => response.text())
+    .then(data => {
+        editablePre.innerText = data;
+    });
+
+editablePre.addEventListener('input', () => {
+    ws.send(editablePre.innerText);
+});
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const WebSocket = require('ws');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// Используем корневую папку проекта для статических файлов
+app.use(express.static(__dirname));
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        // Запись изменений в index.html
+        fs.writeFile(path.join(__dirname, 'swarm_docker_secret.html'), message, (err) => {
+            if (err) console.error(err);
+        });
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Сервер запущен на http://localhost:3000');
+});
+
